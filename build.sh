d05eda0e4939364f5e586d539f7770be2bc34515
cbspace <24204459+cbspace@users.noreply.github.com>
2022-08-15 16:55:08 +1000

    build.sh script updated

--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-if [ $1 == '-configure' ]
+usage_message='Usage Options:\n./build.sh -configure\n./build.sh -build\n./build.sh -run'
+
+if [ $# -eq 0 ]
 then
-  cmake --no-warn-unused-cli \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
-  -DCMAKE_BUILD_TYPE:STRING=Debug \
-  -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc \
-  -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ \
-  -S./ -B./build \
-  -G "Unix Makefiles"
-elif [ $1 == '-build' ]
-then
-  cmake --build ./build --config Debug --target clean -j 26
-  cmake --build ./build --config Debug --target all -j 26
-elif [ $1 == '-run' ]
-then
-  cmake --build ./build --config Debug --target clean -j 26
-  cmake --build ./build --config Debug --target all -j 26
-  ./build/gameboy $2 $3 $4 $5 $6
+  echo -e $usage_message
 else
-  echo 'Usage: "./build.sh -configure" or "./build.sh -run"'
+  if [ $1 == '-configure' ]
+  then
+    cmake --no-warn-unused-cli \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+    -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc \
+    -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ \
+    -S./ -B./build \
+    -G "Unix Makefiles"
+  elif [ $1 == '-build' ]
+  then
+    cmake --build ./build --config Debug --target clean -j 26
+    cmake --build ./build --config Debug --target all -j 26
+  elif [ $1 == '-run' ]
+  then
+    cmake --build ./build --config Debug --target clean -j 26
+    cmake --build ./build --config Debug --target all -j 26
+    ./build/gameboy $2 $3 $4 $5 $6
+  else
+    echo -e $usage_message
+  fi
 fi
