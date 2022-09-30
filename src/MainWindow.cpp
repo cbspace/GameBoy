122b24592efae2963c923a31977cb8c3fa609005
cbspace <24204459+cbspace@users.noreply.github.com>
2022-10-01 08:02:56 +1000

    Menu items added

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -6,13 +6,70 @@ MainWindow::MainWindow() :
     setWindowTitle("EmuBoy");
     setCentralWidget(&gbview);
 
-    auto* file_menu = menuBar()->addMenu("File");
-    auto* view_menu = menuBar()->addMenu("View");
-    auto* about_menu = menuBar()->addMenu("About");
+    create_actions();
+    create_menus();
+    //statusBar->showMessage("hello");
 
     gbview.start_emulator();
 
     timer = new QTimer(this);
     connect(timer, &QTimer::timeout, &gbview, &GameBoyView::animate);
     timer->start(16);
+}
+
+void MainWindow::create_menus() {
+    file_menu = menuBar()->addMenu("File");
+    file_menu->addAction(open_act);
+
+    view_menu = menuBar()->addMenu("View");
+    view_menu->addAction(scale1x_act);
+    view_menu->addAction(scale2x_act);
+
+    about_menu = menuBar()->addMenu("About");
+}
+
+void MainWindow::create_actions() {
+    open_act = new QAction(tr("&Open"), this);
+    open_act->setShortcuts(QKeySequence::Open);
+    open_act->setStatusTip(tr("Open a file"));
+    connect(open_act, &QAction::triggered, this, &MainWindow::open);
+
+    scale1x_act = new QAction(tr("&Scale 1x"), this);
+    //scale1x_act->setShortcuts();
+    scale1x_act->setStatusTip(tr("Scale view 1x"));
+    connect(scale1x_act, &QAction::triggered, this, &MainWindow::scale1x);
+
+    scale2x_act = new QAction(tr("&Scale 2x"), this);
+    //scale2x_act->setShortcuts();
+    scale2x_act->setStatusTip(tr("Scale view 2x"));
+    connect(scale2x_act, &QAction::triggered, this, &MainWindow::scale2x);
+}
+
+void MainWindow::contextMenuEvent(QContextMenuEvent* event) {
+
+}
+
+void MainWindow::file() {
+
+
+}
+void MainWindow::open() {
+
+
+}
+void MainWindow::view() {
+
+
+}
+void MainWindow::scale1x() {
+
+
+}
+void MainWindow::scale2x() {
+
+
+}
+void MainWindow::about() {
+
+
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -8,19 +8,35 @@
 #include <QTimer>
 
 
-namespace GBEmulator {
-
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
     public:
         MainWindow();
 
+    protected:
+        void contextMenuEvent(QContextMenuEvent* event) override;
+
+    private slots:
+        void file();
+        void open();
+        void view();
+        void scale1x();
+        void scale2x();
+        void about();
+
     private:
+        void create_actions();
+        void create_menus();
+
+        QMenu* file_menu;
+        QMenu* view_menu;
+        QMenu* about_menu;
+        QAction* open_act;
+        QAction* scale1x_act;
+        QAction* scale2x_act;
+
         GameBoyView gbview;
         QTimer* timer;
+
 };
-
-}
-
-using GBEmulator::MainWindow;
